ee5989de209d2e56ed63141cfc444e87211a188f
sunny_an <516756148@qq.com>
2019-06-03 15:57:23 +0800

    Add DropBlock3D

--- nets/dropblock.py
+++ nets/dropblock.py
@@ -4,29 +4,35 @@ import tensorflow as tf
 from tensorflow.python.keras import backend as K
 
 
-class DropBlock(tf.keras.layers.Layer):
-    def __init__(self, keep_prob, block_size, **kwargs):
-        super(DropBlock, self).__init__(**kwargs)
+def _bernoulli(shape, mean):
+    return tf.nn.relu(tf.sign(mean - tf.random_uniform(shape, minval=0, maxval=1, dtype=tf.float32)))
+
+
+class DropBlock2D(tf.keras.layers.Layer):
+    def __init__(self, keep_prob, block_size, scale=True, **kwargs):
+        super(DropBlock2D, self).__init__(**kwargs)
         self.keep_prob = float(keep_prob) if isinstance(keep_prob, int) else keep_prob
         self.block_size = int(block_size)
+        self.scale = tf.constant(scale, dtype=tf.bool) if isinstance(scale, bool) else scale
 
     def compute_output_shape(self, input_shape):
         return input_shape
 
     def build(self, input_shape):
+        assert len(input_shape) == 4
         _, self.h, self.w, self.channel = input_shape.as_list()
         # pad the mask
-        bottom = right = (self.block_size - 1) // 2
-        top = left = (self.block_size - 1) - bottom
-        self.padding = [[0, 0], [top, bottom], [left, right], [0, 0]]
+        p1 = (self.block_size - 1) // 2
+        p0 = (self.block_size - 1) - p1
+        self.padding = [[0, 0], [p0, p1], [p0, p1], [0, 0]]
         self.set_keep_prob()
-        super(DropBlock, self).build(input_shape)
+        super(DropBlock2D, self).build(input_shape)
 
-    def call(self, inputs, training=None, scale=True, **kwargs):
+    def call(self, inputs, training=None, **kwargs):
         def drop():
             mask = self._create_mask(tf.shape(inputs))
             output = inputs * mask
-            output = tf.cond(tf.constant(scale, dtype=tf.bool) if isinstance(scale, bool) else scale,
+            output = tf.cond(self.scale,
                              true_fn=lambda: output * tf.to_float(tf.size(mask)) / tf.reduce_sum(mask),
                              false_fn=lambda: output)
             return output
@@ -51,13 +57,66 @@ class DropBlock(tf.keras.layers.Layer):
                                        self.h - self.block_size + 1,
                                        self.w - self.block_size + 1,
                                        self.channel])
-        mask = DropBlock._bernoulli(sampling_mask_shape, self.gamma)
+        mask = _bernoulli(sampling_mask_shape, self.gamma)
         mask = tf.pad(mask, self.padding)
         mask = tf.nn.max_pool(mask, [1, self.block_size, self.block_size, 1], [1, 1, 1, 1], 'SAME')
         mask = 1 - mask
         return mask
 
-    @staticmethod
-    def _bernoulli(shape, mean):
-        return tf.nn.relu(tf.sign(mean - tf.random_uniform(shape, minval=0, maxval=1, dtype=tf.float32)))
+
+class DropBlock3D(tf.keras.layers.Layer):
+    def __init__(self, keep_prob, block_size, scale=True, **kwargs):
+        super(DropBlock3D, self).__init__(**kwargs)
+        self.keep_prob = float(keep_prob) if isinstance(keep_prob, int) else keep_prob
+        self.block_size = int(block_size)
+        self.scale = tf.constant(scale, dtype=tf.bool) if isinstance(scale, bool) else scale
+
+    def compute_output_shape(self, input_shape):
+        return input_shape
+
+    def build(self, input_shape):
+        assert len(input_shape) == 5
+        _, self.d, self.h, self.w, self.channel = input_shape.as_list()
+        # pad the mask
+        p1 = (self.block_size - 1) // 2
+        p0= (self.block_size - 1) - p1
+        self.padding = [[0, 0], [p0, p1], [p0, p1], [p0, p1], [0, 0]]
+        self.set_keep_prob()
+        super(DropBlock3D, self).build(input_shape)
+
+    def call(self, inputs, training=None, **kwargs):
+        def drop():
+            mask = self._create_mask(tf.shape(inputs))
+            output = inputs * mask
+            output = tf.cond(self.scale,
+                             true_fn=lambda: output * tf.to_float(tf.size(mask)) / tf.reduce_sum(mask),
+                             false_fn=lambda: output)
+            return output
+
+        if training is None:
+            training = K.learning_phase()
+        output = tf.cond(tf.logical_or(tf.logical_not(training), tf.equal(self.keep_prob, 1.0)),
+                         true_fn=lambda: inputs,
+                         false_fn=drop)
+        return output
+
+    def set_keep_prob(self, keep_prob=None):
+        """This method only supports Eager Execution"""
+        if keep_prob is not None:
+            self.keep_prob = keep_prob
+        d, w, h = tf.to_float(self.d), tf.to_float(self.w), tf.to_float(self.h)
+        self.gamma = ((1. - self.keep_prob) * (d * w * h) / (self.block_size ** 3) /
+                      ((d - self.block_size + 1) * (w - self.block_size + 1) * (h - self.block_size + 1)))
+
+    def _create_mask(self, input_shape):
+        sampling_mask_shape = tf.stack([input_shape[0],
+                                        self.d - self.block_size + 1,
+                                        self.h - self.block_size + 1,
+                                        self.w - self.block_size + 1,
+                                        self.channel])
+        mask = _bernoulli(sampling_mask_shape, self.gamma)
+        mask = tf.pad(mask, self.padding)
+        mask = tf.nn.max_pool3d(mask, [1, self.block_size, self.block_size, self.block_size, 1], [1, 1, 1, 1, 1], 'SAME')
+        mask = 1 - mask
+        return mask
 
